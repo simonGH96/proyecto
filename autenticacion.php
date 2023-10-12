9d79ccc22a09ba7851da0f3d5fee36cbd05047be
simonGH96 <simoningenieria96@gmail.com>
2023-10-11 23:41:27 -0500

    n
    nn

--- autenticacion.php
+++ autenticacion.php
@@ -1,4 +1,18 @@
 <?php
+
+/*$servername = "localhost"; // Dirección del servidor de la base de datos (puede variar)
+$username_db = "tu_usuario_de_bd"; // Tu nombre de usuario de la base de datos
+$password_db = "tu_contraseña_de_bd"; // Tu contraseña de la base de datos
+$database = "nombre_de_tu_base_de_datos"; // Nombre de la base de datos
+
+// Establecer la conexión
+$conn = new mysqli($servername, $username_db, $password_db, $database);
+
+// Verificar la conexión
+if ($conn->connect_error) {
+    die("Error de conexión a la base de datos: " . $conn->connect_error);
+}
+*/
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $username = $_POST["username"];
     $password = $_POST["password"];
