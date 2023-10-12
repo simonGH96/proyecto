1c5505fdf9248a6bb1bf069321b76b7e59cda966
simonGH96 <simoningenieria96@gmail.com>
2023-10-11 22:48:16 -0500

    create autenticacion
    archivo autenticacion y edicion en prueba.html

---
+++ autenticacion.php
@@ -0,0 +1,19 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $username = $_POST["username"];
+    $password = $_POST["password"];
+
+    // Verifica las credenciales (esto es solo un ejemplo, deberías usar una base de datos y encriptar contraseñas en una aplicación real)
+    $valid_username = "simon"; // Reemplaza con tu nombre de usuario válido
+    $valid_password = "login"; // Reemplaza con tu contraseña válida
+
+    if ($username === $valid_username && $password === $valid_password) {
+        // Autenticación exitosa, redirige a la página de bienvenida o el panel de control
+        header("Location: bienvenida.php"); // Reemplaza con la URL correcta
+        exit();
+    } else {
+        // Autenticación fallida, muestra un mensaje de error
+        echo "Credenciales incorrectas. Por favor, inténtalo de nuevo.";
+    }
+}
+?>
