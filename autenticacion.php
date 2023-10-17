f2b7410bde5c4745836086a7af48dd0e8223950d
simonGH96 <simoningenieria96@gmail.com>
2023-10-16 19:53:34 -0500

    nuevos archivos
    se agrego registro

--- autenticacion.php
+++ autenticacion.php
@@ -1,9 +1,9 @@
 <?php
 
-/*$servername = "localhost"; // Dirección del servidor de la base de datos (puede variar)
-$username_db = "tu_usuario_de_bd"; // Tu nombre de usuario de la base de datos
-$password_db = "tu_contraseña_de_bd"; // Tu contraseña de la base de datos
-$database = "nombre_de_tu_base_de_datos"; // Nombre de la base de datos
+$servername = "localhost"; // Dirección del servidor de la base de datos (puede variar)
+$username_db = "root"; // Tu nombre de usuario de la base de datos
+$password_db = ""; // Tu contraseña de la base de datos
+$database = "proyecto_consejerias"; // Nombre de la base de datos
 
 // Establecer la conexión
 $conn = new mysqli($servername, $username_db, $password_db, $database);
@@ -12,17 +12,18 @@ $conn = new mysqli($servername, $username_db, $password_db, $database);
 if ($conn->connect_error) {
     die("Error de conexión a la base de datos: " . $conn->connect_error);
 }
-*/
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $username = $_POST["username"];
     $password = $_POST["password"];
 
-    // Verifica las credenciales (esto es solo un ejemplo, deberías usar una base de datos y encriptar contraseñas en una aplicación real)
-    $valid_username = "simon"; // Reemplaza con tu nombre de usuario válido
-    $valid_password = "login"; // Reemplaza con tu contraseña válida
+    // Consultar la base de datos para verificar las credenciales
+    $query = "SELECT * FROM Docente WHERE codigo = '$username' AND password = '$password'";
+    $result = $conn->query($query);
 
-    if ($username === $valid_username && $password === $valid_password) {
-        // Autenticación exitosa, redirige a la página de bienvenida o el panel de control
+    if ($result->num_rows > 0) {
+        // Autenticación exitosa
+        // Redirige al usuario a la página de inicio o el panel de control
         header("Location: home.php"); // Reemplaza con la URL correcta
         exit();
     } else {
@@ -30,4 +31,5 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         echo "Credenciales incorrectas. Por favor, inténtalo de nuevo.";
     }
 }
+
 ?>
