5b65d2ec2fb3bb1dbee98cd3ba2b9419a79bcc2d
Drea1030 <andreaasegura2@gmail.com>
2023-10-20 12:09:36 -0500

    informes

--- Inicio/home.php
+++ Inicio/home.php
@@ -2,7 +2,7 @@
 <html>
 <head>
     <meta charset="UTF-8">
-    <title>Página Educativa</title>
+    <title>Consejerías UD v.1.0</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
     <link rel="stylesheet" type="text/css" href="styles.css">
    
