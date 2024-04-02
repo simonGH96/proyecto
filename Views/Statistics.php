d59f693ee0ab12bbe0619e26d705083c53d7b33b
simonGH96 <simoningenieria96@gmail.com>
2024-04-01 22:04:11 -0500

    copia de estadisticas
    copia

--- Views/Statistics.php
+++ Views/Statistics.php
@@ -1,110 +1,118 @@
-<?php 
+<?php
 require '../Views/header.php';
 ?>
-    <div class="row justify-content-center" id="general-title">
-        <div class="col-11">
-            <h3> Datos estadísticos</h3>
-        </div>
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+
+<div class="row justify-content-center" id="general-title">
+    <div class="col-11">
+
+        <h3>Tratamiento estadístico</h3>
     </div>
-    <div class="row justify-content-center" id="widgets-sts">
-        <div class="col-11">
-            <div class="row">
-                <div class="col-lg-3 col-6">
-                    <div class="small-box bg-secondary shadow">
-                        <div class="inner">
-                            <h3>5</h3>
-                            
-                            <p>Resultados de aprendizaje</p>
-                        </div>
-                        <div class="icon">
-                            <i class="fa fa-solid fa-graduation-cap fa-3x"></i>
-                        </div>
+</div>
+<!-- Cuadritos informativos -->
+<div class="row justify-content-center" id="widgets-sts">
+    <div class="col-11">
+        <div class="row">
+            <div class="col-lg-3 col-6">
+                <div class="small-box bg-secondary shadow">
+                    <div class="inner">
+                        <h3>85</h3>
+                        <p class="white-text">Estudiantes activos</p>
+                    </div>
+                    <div class="icon">
+                        <i class="fa fa-solid fa-graduation-cap fa-3x"></i>
                     </div>
                 </div>
-                <div class="col-lg-3 col-6">
-                    <div class="small-box bg-success shadow">
-                        <div class="inner">
-                            <h3>5</h3>
-                            <p>Espacios académicos</p>
-                        </div>
-                        <div class="icon">
-                            <i class="fa fa-solid fa-brain fa-3x"></i>
-                        </div>
+            </div>
+
+
+            <div class="col-lg-3 col-6">
+                <div class="small-box bg-success shadow">
+                    <div class="inner">
+                        <h3>77</h3>
+                        <p class="white-text">Espacios académicos</p>
+                    </div>
+                    <div class="icon">
+                        <i class="fa fa-solid fa-arrow-circle-o-up fa-3x"></i>
                     </div>
                 </div>
-                <div class="col-lg-3 col-6">
-                    <div class="small-box bg-danger shadow">
-                        <div class="inner">
-                            <h3>5</h3>
-                            <p>Docentes</p>
-                        </div>
-                        <div class="icon">
-                            <i class="fa fa-solid fa-person-chalkboard fa-3x"></i>
-                        </div>
+            </div>
+            <div class="col-lg-3 col-6">
+                <div class="small-box bg-danger shadow">
+                    <div class="inner">
+                        <h3>50</h3>
+                        <p class="white-text">Docentes</p>
+                    </div>
+                    <div class="icon">
+                        <i class="fa fa-solid fa-male fa-3x"></i>
                     </div>
                 </div>
-                <div class="col-lg-3 col-6">
-                    <div class="small-box bg-warning shadow">
-                        <div class="inner">
-                            <h3>5</h3>
-                            <p>Asignaciones</p>
-                        </div>
-                        <div class="icon">
-                            <i class="fa fa-solid fa-arrow-right-arrow-left fa-3x"></i>
-                        </div>
+            </div>
+            <div class="col-lg-3 col-6">
+                <div class="small-box bg-warning shadow">
+                    <div class="inner">
+                        <h3>90</h3>
+                        <p>Planes de trabajo</p>
+                    </div>
+                    <div class="icon">
+                        <i class="fa fa-solid fa-book fa-3x"></i>
                     </div>
                 </div>
             </div>
         </div>
     </div>
-    <div class="row justify-content-center">
-        <div class="col-11">
-            <div class="row" id="charts">
-                <div class="col-6">
-                    <div class="card">
-                        <div class="card-body">
-                            <div id="LR_component"></div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-6">
-                    <div class="card">
+</div>
+
+<!-- Gráficos -->
+<div class="row justify-content-center">
+    <div class="col-11">
+        <div class="row" id="charts">
+            <div class="col-6">
+                <div class="card">
                     <div class="card-body">
-                            <div id="container"></div>
-                        </div>
+                        <div id="Grafica1"></div>
                     </div>
                 </div>
             </div>
-
-            <div class="row" id="charts">
-                <div class="col-12">
-                    <div class="card">
-                        <div class="card-body">
-                            <div id="LR_teacher_subject"></div>
-                        </div>
+            <div class="col-6">
+                <div class="card">
+                    <div class="card-body">
+                        <div id="container"></div>
                     </div>
                 </div>
             </div>
-
-            <div class="row" id="charts">
-                <div class="col-12">
-                    <div class="card">
-                        <div class="card-body">
-                            <div id="LR_component_subject"></div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
         </div>
-    </div>
-       
-    <footer>
-        <?php
-        require '../Models/Footer.php';
-    ?>
-    </footer>
 
+        <div class="row" id="charts">
+            <div class="col-12">
+                <div class="card">
+                    <div class="card-body">
+                        <div id="Grafica2"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="row" id="charts">
+            <div class="col-12">
+                <div class="card">
+                    <div class="card-body">
+                        <div id="Grafica3"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+</div>
+
+<!-- Enlaza a la biblioteca Highcharts desde un CDN -->
 <script src="https://code.highcharts.com/highcharts.js"></script>
 <script src="https://code.highcharts.com/modules/data.js"></script>
 <script src="https://code.highcharts.com/modules/drilldown.js"></script>
@@ -117,63 +125,116 @@ require '../Views/header.php';
 <script src="https://code.highcharts.com/modules/accessibility.js"></script>
 <script src="https://code.highcharts.com/modules/treemap.js"></script>
 
-<script>
-    Highcharts.setOptions({
-        colors: Highcharts.map(Highcharts.getOptions().colors, function (color) {
-            return {
-                radialGradient: {
-                    cx: 0.5,
-                    cy: 0.3,
-                    r: 0.7
+<body>
+    <script>
+    Highcharts.chart('Grafica1', {
+    chart: {
+        type: 'pie'
+    },
+    title: {
+        text: 'Egg Yolk Composition'
+    },
+    tooltip: {
+        valueSuffix: '%'
+    },
+    subtitle: {
+        text:
+        'Source:<a href="https://www.mdpi.com/2072-6643/11/3/684/htm" target="_default">MDPI</a>'
+    },
+    plotOptions: {
+        series: {
+            allowPointSelect: true,
+            cursor: 'pointer',
+            dataLabels: [{
+                enabled: true,
+                distance: 20
+            }, {
+                enabled: true,
+                distance: -40,
+                format: '{point.percentage:.1f}%',
+                style: {
+                    fontSize: '1.2em',
+                    textOutline: 'none',
+                    opacity: 0.7
                 },
-                stops: [
-                    [0, color],
-                    [1, Highcharts.color(color).brighten(-0.3).get('rgb')] 
-                ]
-            };
-        })
-    });
-
-    Highcharts.chart('LR_component', {
-        chart: {
-            plotBackgroundColor: null,
-            plotBorderWidth: null,
-            plotShadow: false,
-            type: 'pie'
-        },
-        title: {
-            text: 'Resultados de aprendizaje asignados por componente'
-        },
-        tooltip: {
-            pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
-        },
-        accessibility: {
-            point: {
-                valueSuffix: '%'
-            }
-        },
-        plotOptions: {
-            pie: {
-                allowPointSelect: true,
-                cursor: 'pointer',
-                dataLabels: {
-                    enabled: true,
-                    format: '<b>{point.name}</b>: {point.percentage:.1f} %',
-                    connectorColor: 'silver'
+                filter: {
+                    operator: '>',
+                    property: 'percentage',
+                    value: 10
                 }
-            }
-        },
-        series: [{
-            name: 'Resaultados de aprendizaje',
+            }]
+        }
+    },
+    series: [
+        {
+            name: 'Percentage',
+            colorByPoint: true,
             data: [
-                <?php foreach($data['learning_result_component'] as $value){
-                    echo "{name:'".$value['nombre']."',y:".$value['amount']."},";
-                } ?>
+                {
+                    name: 'Water',
+                    y: 55.02
+                },
+                {
+                    name: 'Fat',
+                    sliced: true,
+                    selected: true,
+                    y: 26.71
+                },
+                {
+                    name: 'Carbohydrates',
+                    y: 1.09
+                },
+                {
+                    name: 'Protein',
+                    y: 15.5
+                },
+                {
+                    name: 'Ash',
+                    y: 1.68
+                }
             ]
-        }]
-    });
+        }
+    ]
+});
 
 </script>
+
+<script >
+        // Datos de ejemplo (puedes reemplazar esto con tus propios datos)
+        var dataFromDatabase = {
+            categories: ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo'],
+            values: [10, 20, 15, 25, 30]
+        };
+
+        // Configura las opciones del gráfico
+        var options = {
+            chart: {
+                type: 'bar'
+            },
+            title: {
+                text: 'Ejemplo de Gráfico Highcharts'
+            },
+            xAxis: {
+                categories: dataFromDatabase.categories // Categorías del eje X
+            },
+            yAxis: {
+                title: {
+                    text: 'Valores'
+                }
+            },
+            series: [{
+                name: 'Datos de la BD',
+                data: dataFromDatabase.values // Valores para el gráfico
+            }, {
+                name: 'Datos 2',
+                data: [5, 6, 13, 24, 5]
+            }]
+        };
+
+        // Renderiza el gráfico en el contenedor "container"
+        Highcharts.chart('Grafica2', options);
+    </script>
+
 
 <script>
         // Set up the chart
@@ -212,70 +273,37 @@ require '../Views/header.php';
         series: [{
             name: 'Asignaturas',
             data: [
-                <?php foreach($data['subject_component'] as $value){
-                    echo "['".$value['nombre']."',".$value['amount']."],";
-                } ?>
+                
+                {
+                    name: 'Water',
+                    y: 55.02
+                },
+                {
+                    name: 'Fat',
+                    sliced: true,
+                    selected: true,
+                    y: 26.71
+                },
+                {
+                    name: 'Carbohydrates',
+                    y: 1.09
+                },
+                {
+                    name: 'Protein',
+                    y: 15.5
+                },
+                {
+                    name: 'Ash',
+                    y: 1.68
+                }
             ]
         }]
     });
 </script>
-
-<script>
-    Highcharts.chart('LR_teacher_subject', {
-        chart: {
-            type: 'spline'
-        },
-        title: {
-            text: 'Resultados de aprendizaje - Docentes y Asignaturas'
-        },
-        xAxis: {
-            categories: [<?php foreach($data['learning_result_teacher_subject'] as $value){
-                        echo "'".$value['descripcion']."',";
-                    } ?>]
-        },
-        yAxis: {
-            title: {
-                text: 'Cantidad'
-            }
-        },
-        tooltip: {
-            crosshairs: true,
-            shared: true
-        },
-        plotOptions: {
-            series: {
-                label: {
-                    connectorAllowed: false
-                },
-                cursor: 'pointer'
-            }
-        },
-        series: [{
-            name: 'Espacios académicos',
-            marker: {
-                symbol: 'square'
-            },
-            data: [<?php foreach($data['learning_result_teacher_subject'] as $value){
-                        echo $value['amount_subject'].",";
-                    } ?>]
-
-        }, {
-            name: 'Docentes',
-            marker: {
-                symbol: 'diamond'
-            },
-            data: [<?php foreach($data['learning_result_teacher_subject'] as $value){
-                        echo $value['amount_teacher'].",";
-                    } ?>]
-        }]
-    });
-
-</script>
-
 <script>
         // Data retrieved from https://gs.statcounter.com/browser-market-share#monthly-202201-202201-bar
     // Create the chart
-    Highcharts.chart('LR_component_subject', {
+    Highcharts.chart('Grafica3', {
         chart: {
             type: 'column'
         },
@@ -320,9 +348,7 @@ require '../Views/header.php';
                 name: "Resultados de aprendizaje",
                 colorByPoint: true,
                 data: [
-                    <?php foreach($data['learning_result_component'] as $value){
-                        echo "{name:'".$value['nombre']."',y:".$value['amount'].",drilldown: '".$value['nombre']."'},";
-                    } ?>
+                    
                 ]
             }
         ],
@@ -333,18 +359,20 @@ require '../Views/header.php';
                 }
             },
             series: [
-                    <?php foreach($data['learning_result_component'] as $value){
-                        echo "{name:'".$value['nombre']."',id:'".$value['nombre']."', data:[";
-                        foreach($data['learning_result_subject_component'] as $key=>$valueComp){
-                            if($valueComp['compo'] == $value['nombre']){
-                                echo "['".$valueComp['nombre']."',".$valueComp['amount']."],";
-                            }
-                        }
-                        echo "]},";
-                    } ?>
+                {
+                    name: 'Water',
+                    y: 55.02
+                },
+                {
+                    name: 'Fat',
+                    sliced: true,
+                    selected: true,
+                    y: 26.71
+                }
             ]
         }
     });
 </script>
+</body>
 
-
+</html>
